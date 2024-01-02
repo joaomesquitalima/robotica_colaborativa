a460b27877397b724967c8c552420e6436d2f3df
João Vitor Mesquita de Lima <99664194+joaomesquitalima@users.noreply.github.com>
2024-01-02 15:22:30 -0400

    commit inicial

---
+++ mod2/ex1.py
@@ -0,0 +1,25 @@
+from pyModbusTCP.client import ModbusClient
+from pyModbusTCP import utils
+from time import sleep
+
+SERVER_IP = '192.168.1.252'
+SERVER_PORT = 502
+DEVICE_ID = 1
+
+print('tentando conectar')
+client = ModbusClient(host=SERVER_IP, port=SERVER_PORT)
+
+if client.open():
+    print('Connetion to Robot %s:%d established sucesso' %(SERVER_IP,SERVER_PORT))
+
+else:
+    print('erro')
+    exit()
+
+play_pause = client.write_single_coil(7104,1)
+
+sleep(1)
+
+while True:
+    green_button = client.read_discrete_inputs(3,1)[0]
+   
